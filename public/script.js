c2262da26bc380886dde71d19eefff96f248fa59
Necro Mancer <necromancer@users.noreply.github.com>
2020-03-22 01:51:33 +0530

    waiting for proxy deployment

--- public/script.js
+++ public/script.js
@@ -16335,7 +16335,7 @@ function read_download(event) {
         $('#read input').focus();
         $('#read input').attr('id', 'present');
       } else {
-        var content = "<div class='box'> <input type='checkbox' id='select-all' /> <span class='check'></span> <label for='select-all'> Select All </label> </div>";
+        var content = "<div class='box'> <input type='checkbox' id='select-all' /> <span class='check'></span> <label for='select-all'> SELECT ALL </label> </div>";
         for (i = data.length - 1; i > 0; i -= 2) {
           content += "<div class='box'> <input type='checkbox' class='checkbox' id='" + i + "' name='" + i + "' /> <span class='check'></span> <label for='" + i + "'> " + data[i] + " </label> </div>";
         }
